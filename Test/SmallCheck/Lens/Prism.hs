f6ef967409e47fa9395a688b19f75f935a939104
Danny Navarro <j@dannynavarro.net>
2015-09-10 15:39:27 +0200

    Rename `Prism` functions assuming qualified imports

--- Test/SmallCheck/Lens/Prism.hs
+++ Test/SmallCheck/Lens/Prism.hs
@@ -1,4 +1,8 @@
 {-# LANGUAGE RankNTypes #-}
+-- | This module is intended to be imported @qualified@, for example:
+--
+-- > import qualified Test.SmallCheck.Lens.Prism as Prism
+--
 module Test.SmallCheck.Lens.Prism where
 
 import Control.Lens
@@ -6,12 +10,12 @@ import Test.SmallCheck (Property)
 import qualified Test.SmallCheck as SC (over)
 import Test.SmallCheck.Series (Series)
 
-prismYin
+yin
   :: (Monad m, Eq s, Show s, Eq a, Show a)
   => Prism' s a -> Series m a -> Property m
-prismYin l as = SC.over as $ \a -> preview l (review l a) == Just a
+yin l as = SC.over as $ \a -> preview l (review l a) == Just a
 
-prismYang
+yang
   :: (Monad m, Eq s, Show s, Eq a, Show a)
   => Prism' s a -> Series m s -> Property m
-prismYang l ss = SC.over ss $ \s -> maybe s (review l) (preview l s) == s
+yang l ss = SC.over ss $ \s -> maybe s (review l) (preview l s) == s
